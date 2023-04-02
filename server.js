a8e60e14c83c2736c035acde406b9d37f844f116
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-02 17:28:58 +0530

    seted up the views and public directory

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const ejs = require('ejs');
+const path = require('path')
+
+app.set('views', ejs)
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res)=>{
     res.send("hello");
