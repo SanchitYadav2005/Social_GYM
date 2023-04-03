124b7e041af2684854e284fa7279361d6ab25931
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-03 20:23:13 +0530

    trying to get it work - throwing error that is body is undefined

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/view'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended:true}))
 
