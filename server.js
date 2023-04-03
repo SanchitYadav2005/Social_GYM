dbd85ff28981e3f7d884081b19d4cdd19a7fd0be
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-03 20:15:15 +0530

    changed layout little bit

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const port = 8080;
 const ejs = require('ejs');
 const path = require('path')
 
-app.set('views', ejs)
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:true}))
 
 app.get('/', (req,res)=>{
-    res.send("hello");
+    res.render('pages/home')
 })
 
 app.listen(port, ()=>{
