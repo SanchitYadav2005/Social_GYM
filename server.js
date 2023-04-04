7bbf0ff9fca784b6c19a4dea5d2a80af39503b9b
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-04 21:25:51 +0530

    added ejsMate in the project

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const ejs = require('ejs');
+const ejsMate = require("ejs-mate")
 const path = require('path')
 
-
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/view'));
 app.use(express.static(path.join(__dirname, 'public')));
