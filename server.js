4ae7c0bd9bb2bac63d6c65f0fd91ffd07b43d3a3
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-23 19:46:29 +0530

    server started working

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const UserSchema = require('./Schemas/userSchema');
 const mongoose = require('mongoose');
 
 
-mongoose.connect("mongodb://localhost:27017/socialGym");
+mongoose.connect('mongodb://127.0.0.1:27017/socialGym');
 
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
