7c5316c90abf820448eb57d573670dbbd318c355
unknown <sanchit2005yudavanshi@gmail.com>
2023-04-25 22:58:05 +0530

    it is working but it is not letting the user get in

--- Schemas/userSchema.js
+++ Schemas/userSchema.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
     fname: String,
     lname: String,
     username: String,
     passowrd: String
-})
+});
 
+userSchema.plugin(passportLocalMongoose)
 
-
-module.exports = mongoose.model('UserSchema', userSchema);;
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 8080;
 const ejsMate = require("ejs-mate")
 const path = require('path');
-const UserSchema = require('./Schemas/userSchema');
+const User = require('./Schemas/userSchema');
 const mongoose = require('mongoose');
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const session = require("express-session");
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/socialGym');
@@ -13,7 +16,18 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/view'));
 app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
+app.use(session({
+    secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', (req,res)=>{
     res.render('pages/home')
@@ -23,7 +37,7 @@ app.get('/signUp', (req,res)=>{
 });
 app.post('/signUp', async (req,res)=>{
     const {id} = req.params;
-    const user = new UserSchema(req.body);
+    const user = new User(req.body);
     await user.save();
     res.redirect(`/${id}/dashboard`);
     console.log(user)
@@ -31,10 +45,17 @@ app.post('/signUp', async (req,res)=>{
 app.get('/signIn', (req,res)=>{
     res.render('pages/signIn')
 });
+app.post('/signIn', passport.authenticate('local', {failureRedirect: '/signIn'}), (req,res)=>{
+    console.log(req.body)
+    res.redirect('/dashboard')
+})
 app.get('/:id/dashboard', (req,res)=>{
     const {id} = req.params;
-    const user = UserSchema.findById(id);
+    const user = User.findById(id);
     res.render('pages/dashboard', {user})
+})
+app.get('/dashboard',(req,res)=>{
+    res.send("authencation is working!")
 })
 
 app.listen(port, ()=>{
