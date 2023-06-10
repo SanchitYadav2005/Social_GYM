90697ca42433bf771d7f7b4def4eba2c383345b6
unknown <sanchit2005yudavanshi@gmail.com>
2023-06-10 11:26:47 +0530

    configured the session

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ExpressError = require('./utils/expressError');
 const userRoutes = require('./routes/userRoutes');
+const session = require('express-session');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/socialGym');
@@ -15,6 +16,26 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/view'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({extended:true}));
+
+const sessionConfig = {
+    name: 'session',
+    secret: 'she is mine',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+
+
+
+
+
+
 app.use('/', userRoutes)
 
 app.get('/', (req,res)=>{
