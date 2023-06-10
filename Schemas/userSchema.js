4ddef5871d3813222946d47f254354413148b84c
unknown <sanchit2005yudavanshi@gmail.com>
2023-06-10 11:40:20 +0530

    added the passport-local-mongoose as plugin in the schema

--- Schemas/userSchema.js
+++ Schemas/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const userSchema = new Schema({
@@ -8,6 +9,8 @@ const userSchema = new Schema({
     username: String,
     passowrd: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 
 
