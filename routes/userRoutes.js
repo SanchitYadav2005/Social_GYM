eaffc232cfbd8e30b96ddc56586e32b5797567c6
unknown <sanchit2005yudavanshi@gmail.com>
2023-06-11 15:01:48 +0530

    removed exrta stuff

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router()
 const userController = require('../controllers/UserController');
-const passort = require('passport');
 const passport = require('passport');
 
 router.route('/signUp')
