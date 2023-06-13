a4dd3a0130760424d04ecc3427765284d6ac5bc5
unknown <sanchit2005yudavanshi@gmail.com>
2023-06-13 11:28:27 +0530

    woring on create post form

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/posts', (req,res)=>{
+    res.send(" posts will display here!")
+})
+router.get("/createPost", (req,res)=>{
+    res.send("you post will be created here")
+})
+
 module.exports = router;
