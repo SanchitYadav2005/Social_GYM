93ba1f3a70e4036b5cfcd97359eaf26d94123e3a
unknown <sanchit2005yudavanshi@gmail.com>
2023-06-14 11:18:19 +0530

    created a schema for posts

---
+++ Schemas/postSchema.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const postSchmea = new Schema({
+    title: String,
+    description: String
+});
+
+module.exports = mongoose.model('PostSchmea', postSchmea)
