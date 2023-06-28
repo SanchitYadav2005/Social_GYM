a2a16012a85c25a14778bbb6f7456b7b8fed2b46
unknown <sanchit2005yudavanshi@gmail.com>
2023-06-28 05:58:21 +0530

    images is uploading in the uploads folder

--- controllers/PostControllers.js
+++ controllers/PostControllers.js
@@ -1,12 +1,14 @@
 const catchAsync = require('../utils/catchAsync');
 const Post = require('../Schemas/postSchema');
 
+
 module.exports.sendPostForm = (req,res)=>{  
     res.render('pages/createPost');
 }
 
 module.exports.createPost = catchAsync(async (req,res)=>{
     const post = new Post(req.body)
+    post.images = req.files.map(f => ({url: f.path, filename: f.filename}));
     await post.save()
     res.redirect('/posts')
 })
