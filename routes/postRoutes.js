db1cdf1bf3727099abf9086f32994027adf2520f
unknown <sanchit2005yudavanshi@gmail.com>
2023-06-28 05:46:31 +0530

    added upload.arrary into our post route for the image

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/PostControllers');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'})
 
 router.get('/posts', (req,res)=>{
     res.render('pages/posts')
 })
 router.get('/createPost', postController.sendPostForm);
-router.post('/createPost', postController.createPost);
+router.post('/createPost',upload.array('images'), postController.createPost);
 
 module.exports = router;
