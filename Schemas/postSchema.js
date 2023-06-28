9ef7e14bfdeec3461505aad13c1a7be49f9d903d
unknown <sanchit2005yudavanshi@gmail.com>
2023-06-28 05:52:43 +0530

    added images schema saperately in posts

--- Schemas/postSchema.js
+++ Schemas/postSchema.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 const {Schema} = mongoose;
 
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
 const postSchmea = new Schema({
+    image: [ImageSchema],
     title: String,
     description: String
 });
